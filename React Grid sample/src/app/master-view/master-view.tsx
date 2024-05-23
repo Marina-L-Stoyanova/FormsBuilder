0abe3e29f55e633c77a71393dc3ac82044473c8c
Alexander M <niron_hsc@yahoo.com>
2024-05-23 18:54:03 +0300

    Updating sample

--- React Grid sample/src/app/master-view/master-view.tsx
+++ React Grid sample/src/app/master-view/master-view.tsx
@@ -1,10 +1,10 @@
-import { Form, IgrButton, IgrDialog, IgrButtonModule, IgrDialogModule, IgrInput, IgrInputModule, IgrTextareaModule } from "@infragistics/igniteui-react";
+import { IgrButton, IgrButtonModule, IgrDialog, IgrDialogModule, IgrInput, IgrInputModule, IgrTextareaModule } from "@infragistics/igniteui-react";
 import { IgrColumn, IgrGrid, IgrGridModule } from '@infragistics/igniteui-react-grids';
-import { useGetCustomerInputModelList } from '../hooks/north-wind-crud-hooks';
 import '@infragistics/igniteui-react-grids/grids';
-import styles from './master-view.module.css';
+import { useReducer, useRef } from "react";
+import { useGetCustomerInputModelList } from '../hooks/north-wind-crud-hooks';
 import createClassTransformer from '../style-utils';
-import React, { useReducer, useRef } from "react";
+import styles from './master-view.module.css';
 
 IgrButtonModule.register();
 IgrDialogModule.register();
@@ -14,16 +14,14 @@ IgrTextareaModule.register();
 
 export default function MasterView() {
   const classes = createClassTransformer(styles);
-  const uuid = () => crypto.randomUUID();
   const { northWindCRUDCustomerInputModel } = useGetCustomerInputModelList();
-  const dialogRef = React.useRef<IgrDialog>(null);
+  const dialogRef = useRef<IgrDialog>(null);
   const [, forceUpdate] = useReducer(x => x + 1, 0);
-
   const formElement = useRef<HTMLFormElement>(null);
 
-  const next = () =>{
+  const next = () => {
     forceUpdate();
-  }
+  };
 
   const onDialogShow = () => {
     if (dialogRef.current) {
@@ -37,31 +35,51 @@ export default function MasterView() {
     }
   };
 
+  const renderHelperText = (fieldName) => {
+    const field = formElement.current?.elements.namedItem(fieldName);
+    if (field && (field as HTMLInputElement).validity.valueMissing) {
+      return (
+        <span slot="helper-text" className={classes("helper-text")} key={`${fieldName}-helper`}>
+          This field is required
+        </span>
+      );
+    }
+    return null;
+  };
+
   return (
     <>
       <div className={classes("row-layout master-view-container")}>
         <IgrGrid data={northWindCRUDCustomerInputModel} primaryKey="customerId" displayDensity="cosy" allowFiltering="true" filterMode="excelStyleFilter" className={classes("ig-typography ig-scrollbar grid")}>
-          <IgrColumn field="customerId" dataType="string" header="customerId" sortable="true" selectable="false"></IgrColumn>
-          <IgrColumn field="companyName" dataType="string" header="companyName" sortable="true" selectable="false"></IgrColumn>
-          <IgrColumn field="contactName" dataType="string" header="contactName" sortable="true" selectable="false"></IgrColumn>
-          <IgrColumn field="contactTitle" dataType="string" header="contactTitle" sortable="true" selectable="false"></IgrColumn>
-          <IgrColumn field="address.city" dataType="string" header="address city" sortable="true" selectable="false"></IgrColumn>
-          <IgrColumn field="address.country" dataType="string" header="address country" sortable="true" selectable="false"></IgrColumn>
+          <IgrColumn field="customerId" dataType="string" header="customerId" sortable="true" selectable="false" />
+          <IgrColumn field="companyName" dataType="string" header="companyName" sortable="true" selectable="false" />
+          <IgrColumn field="contactName" dataType="string" header="contactName" sortable="true" selectable="false" />
+          <IgrColumn field="contactTitle" dataType="string" header="contactTitle" sortable="true" selectable="false" />
+          <IgrColumn field="address.city" dataType="string" header="address city" sortable="true" selectable="false" />
+          <IgrColumn field="address.country" dataType="string" header="address country" sortable="true" selectable="false" />
         </IgrGrid>
         <IgrDialog title="Confirmation" ref={dialogRef}>
           <form className="form" ref={formElement}>
-            <IgrInput label="Label/Placeholder" outlined="true" name="name" className={classes("user-input")} key={uuid()} required={true} blur={next}>
-              {(formElement?.current?.elements.namedItem("name") as HTMLInputElement)?.validity.valueMissing ? <span slot="helper-text" className={classes("helper-text")} key={uuid()}>This field is required</span> : undefined}
+            <IgrInput label="Customer ID" outlined="true" name="customerId" className={classes("user-input")} key="customerId" blur={next}>
+            </IgrInput>
+            <IgrInput label="Company name" outlined="true" name="companyName" className={classes("user-input")} key="companyName" required={true} blur={next}>
+              {renderHelperText("companyName")}
+            </IgrInput>
+            <IgrInput label="Contact name" outlined="true" name="contactName" className={classes("user-input")} key="contactName" required={true} blur={next}>
+              {renderHelperText("contactName")}
+            </IgrInput>
+            <IgrInput label="Contact title" outlined="true" name="contactTitle" className={classes("user-input")} key="contactTitle" required={true} blur={next}>
+              {renderHelperText("contactTitle")}
             </IgrInput>
           </form>
-        <div slot="footer">
-          <IgrButton clicked={onDialogHide} variant="flat"><span>Cancel</span></IgrButton>
-          <IgrButton clicked={onDialogHide} variant="flat"><span>OK</span></IgrButton>
-        </div>
-      </IgrDialog>
-      <IgrButton variant="contained" clicked={onDialogShow}>
-        <span>Add customer</span>
-      </IgrButton>
+          <div slot="footer">
+            <IgrButton clicked={onDialogHide} variant="flat"><span>Cancel</span></IgrButton>
+            <IgrButton clicked={onDialogHide} variant="flat"><span>OK</span></IgrButton>
+          </div>
+        </IgrDialog>
+        <IgrButton variant="contained" clicked={onDialogShow}>
+          <span>Add customer</span>
+        </IgrButton>
       </div>
     </>
   );
