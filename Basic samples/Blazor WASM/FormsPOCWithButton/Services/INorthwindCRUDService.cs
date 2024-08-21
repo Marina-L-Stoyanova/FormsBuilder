406b18814cab95461eb2d8dcb6b69726ab89379f
Milko Venkov <MVenkov@infragistics.com>
2024-08-21 13:57:25 +0300

    feat(blazor): add create and update forms

--- Basic samples/Blazor WASM/FormsPOCWithButton/Services/INorthwindCRUDService.cs
+++ Basic samples/Blazor WASM/FormsPOCWithButton/Services/INorthwindCRUDService.cs
@@ -4,6 +4,10 @@ namespace FormsPOCWithButton.Services
 {
     public interface INorthwindCRUDService
     {
+        Task<CustomerDto> GetCustomerDto(string? id);
+
         Task<CustomerDto> PostCustomerDto(CustomerDto? data);
+
+        Task<CustomerDto> PutCustomerDto(CustomerDto? data);
     }
 }
--- Basic samples/Blazor WASM/TestFormsPOCWithButton/Services/MockNorthwindCRUDService.cs
+++ Basic samples/Blazor WASM/TestFormsPOCWithButton/Services/MockNorthwindCRUDService.cs
@@ -4,7 +4,17 @@ namespace FormsPOCWithButton.Services
 {
     public class MockNorthwindCRUDService : INorthwindCRUDService
     {
+        public Task<CustomerDto> GetCustomerDto(string id)
+        {
+            return Task.FromResult<CustomerDto>(new());
+        }
+
         public Task<CustomerDto> PostCustomerDto(CustomerDto data)
+        {
+            return Task.FromResult<CustomerDto>(new());
+        }
+
+        public Task<CustomerDto> PutCustomerDto(CustomerDto data)
         {
             return Task.FromResult<CustomerDto>(new());
         }
