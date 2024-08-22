c261278eb4b98a0837e930d59b59484a3fb18c91
Milko Venkov <MVenkov@infragistics.com>
2024-08-22 14:48:08 +0300

    feat(blazor): make models clonable

--- Basic samples/Blazor WASM/FormsPOCWithButton/Models/NorthwindCRUD/AddressDto.cs
+++ Basic samples/Blazor WASM/FormsPOCWithButton/Models/NorthwindCRUD/AddressDto.cs
@@ -1,6 +1,6 @@
 namespace FormsPOCWithButton.Models.NorthwindCRUD;
 
-public class AddressDto
+public class AddressDto: ICloneable
 {
     public string Street { get; set; }
     public string City { get; set; }
@@ -8,4 +8,17 @@ public class AddressDto
     public string PostalCode { get; set; }
     public string Country { get; set; }
     public string Phone { get; set; }
+
+    public object Clone()
+    {
+        return new AddressDto
+        {
+            Street = Street,
+            City = City,
+            Region = Region,
+            PostalCode = PostalCode,
+            Country = Country,
+            Phone = Phone
+        };
+    }
 }
--- Basic samples/Blazor WASM/FormsPOCWithButton/Models/NorthwindCRUD/CustomerDto.cs
+++ Basic samples/Blazor WASM/FormsPOCWithButton/Models/NorthwindCRUD/CustomerDto.cs
@@ -1,10 +1,22 @@
 namespace FormsPOCWithButton.Models.NorthwindCRUD;
 
-public class CustomerDto
+public class CustomerDto: ICloneable
 {
     public string CustomerId { get; set; }
     public string CompanyName { get; set; }
     public string ContactName { get; set; }
     public string ContactTitle { get; set; }
     public AddressDto Address { get; set; } = new();
+
+    public object Clone()
+    {
+        return new CustomerDto
+        {
+            CustomerId = CustomerId,
+            CompanyName = CompanyName,
+            ContactName = ContactName,
+            ContactTitle = ContactTitle,
+            Address = Address.Clone() as AddressDto,
+        };
+    }
 }
