78158630e984c894fff3855db87c57df93dd71a4
Alexander M <niron_hsc@yahoo.com>
2024-09-03 14:14:10 +0300

    Updating sample

--- Basic samples/React Basic sample/src/app/master-view/master-view.tsx
+++ Basic samples/React Basic sample/src/app/master-view/master-view.tsx
@@ -12,30 +12,27 @@ IgrTextareaModule.register();
 export default function MasterView() {
   const classes = createClassTransformer(styles);
   const [, forceUpdate] = React.useReducer(x => x + 1, 0);
-  const formElement = React.useRef<HTMLFormElement>(null);
 
   const next = () => {
     forceUpdate();
   };
 
-  const onAddNewSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); // Prevent the default form submission
+  const onAddNewSubmit = async (args: SubmitEvent) => {
+    args.preventDefault(); // Prevent the default form submission
 
-    if (formElement.current) {
-      const formObject = formDataToObject(formElement.current);
-      try {
-        const data = await northwindCRUDService.postCustomerDto(formObject);
-        // TODO: handle local data update if needed.
-        console.log("Data submitted successfully:", data);
-      } catch (error) {
-        console.error("Error submitting form data:", error);
-      }
+    const formObject = formDataToObject(args.target as HTMLFormElement);
+    try {
+      const data = await northwindCRUDService.postCustomerDto(formObject);
+      // TODO: handle local data update if needed.
+      console.log("Data submitted successfully:", data);
+    } catch (error) {
+      console.error("Error submitting form data:", error);
     }
   };
 
   return (
       <div className={classes("row-layout master-view-container")}>
-          <form className="form" ref={formElement} action="${this.onAddNewSubmit}">
+          <form className="form" onSubmit={onAddNewSubmit}>
             <IgrInput label="Customer ID" outlined="true" name="customerId" className={classes("user-input")} key="customerId" blur={next}>
             </IgrInput>
             <IgrInput label="Company name" outlined="true" name="companyName" className={classes("user-input")} key="companyName" blur={next}>
